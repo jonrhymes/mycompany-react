5bc0ee076b5876c98db86defc48bc8dd4ae17a47
jonrhymes <jonrhymes@gmail.com>
2021-03-05 14:14:14 -0500

    navbar menu complete

--- react-compare/src/components/Footer/Footer.js
+++ react-compare/src/components/Footer/Footer.js
@@ -1,7 +1,22 @@
+import './Footer.scss'
+
 function Footer() {
     return (
         <>
-            <h3>Footer</h3>
+            <footer className="footer">
+                <div className="container">
+                    <div className="columns">
+                        <div className="column">
+                            <p className="foot-desc">Is this worth anything?</p>
+                            <p className="answer">You know what? It is! Email the creator of this website: <strong>jonrhymes@gmail.com</strong> for details. He’s a Developer. Ooo!</p>
+                        </div>
+                        <div className="column has-text-right">
+                            <a className="social-icon" href="https://www.facebook.com/"><i className="fa fa-2x fa-facebook"></i></a>
+                            <a className="social-icon" href="https://twitter.com/"><i className="fa fa-2x fa-twitter"></i></a>
+                        </div>
+                    </div>
+                </div>
+    </footer>
         </>
     );
   }
--- react-compare/src/components/Header/Header.js
+++ react-compare/src/components/Header/Header.js
@@ -1,46 +1,62 @@
-import '../Header/Header.scss'
+import React, { Component } from 'react';
+import './Header.scss'
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 
-function Header() {
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { isToggleOn: false };
 
-    return (
-        <>
-            <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn
+        }));
+    }
+    render() { 
+
+        let menuActive = this.state.isToggleOn ? 'is-active' : '';
+        return (
+            <>
+                <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
                 
-            <div className="navbar-brand">
-                <button id="brand" className="button is-dark is-medium">MyCompany</button>
+                    <div className="navbar-brand">
+                        <button id="brand" className="button is-dark is-medium">MyCompany</button>
 
-                <button class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                </button>
-            </div>
-
-            <div id="navbarBasicExample" className="navbar-menu">
-                    <div className="navbar-end">
-                        <Router forceRefresh={true}>
-                            <Link to="/" className="navbar-item">Home</Link>
-                            <Link to="/faq" className="navbar-item">Features</Link>
-                            <Link to="/faq" className="navbar-item">About</Link>
-                            <Link to="/faq" className="navbar-item">FAQ</Link>
-                        </Router>
-        
-                        <div className="navbar-item">
-                        <div className="buttons">
-                        <button className="button is-primary is-outlined">
-                            <span className="icon">
-                            <i className="fa fa-download"></i>
-                            </span>
-                            <span>Join Now</span>
-                        </button>
-                        </div>
+                        <a role="button" className={"navbar-burger "+menuActive} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" onClick={this.handleClick}>
+                            <span aria-hidden="true"></span>
+                            <span aria-hidden="true"></span>
+                            <span aria-hidden="true"></span>
+                        </a>
                     </div>
-                </div>
-            </div>
-            </nav>
-        </>
-    );
+
+                    <div id="navbarBasicExample" className={"navbar-menu navbar-end "+menuActive}>
+                        {/* <div className="navbar-end"> */}
+                            <Router forceRefresh={true}>
+                                <Link to="/" className="navbar-item">Home</Link>
+                                <Link to="/faq" className="navbar-item">Features</Link>
+                                <Link to="/faq" className="navbar-item">About</Link>
+                                <Link to="/faq" className="navbar-item">FAQ</Link>
+                            </Router>
+        
+                            <div className="navbar-item">
+                                <div className="buttons">
+                                    <button className="button is-primary is-outlined">
+                                        <span className="icon">
+                                            <i className="fa fa-download"></i>
+                                        </span>
+                                        <span>Join Now</span>
+                                    </button>
+                                </div>
+                            </div>
+                        {/* </div> */}
+                    </div>
+                </nav>
+            </>
+        );
+    }
   }
   
   
