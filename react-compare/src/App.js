1f2e580ba7fa6bc7c34313e877ce1237652853dd
jonrhymes <jonrhymes@gmail.com>
2021-03-13 10:26:41 -0500

    restoring master from file copies

---
+++ react-compare/src/App.js
@@ -0,0 +1,25 @@
+import './App.scss';
+import 'bulma';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import Faq from './components/Faq/Faq';
+import { Route, Switch, HashRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <HashRouter>
+        <Switch>
+          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home}><Home /></Route>
+          // Added process.env.PUBLIC_URL to Route for Deployment
+          <Route path={`${process.env.PUBLIC_URL}/faq`} component={Faq}><Faq /></Route>
+        </Switch>
+      </HashRouter>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
